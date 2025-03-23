554f64cce4ea069b6861513b42c96765440753ad
luckyantony <luckyantonyofficial@gmail.com>
2025-03-23 12:22:57 +0300

    Seeing all character names in a div within a span

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 // Your code here
+document.addEventListener("DOMContentLoaded", main);
+
+fetch ("http://localhost:3000/characters")
+.then(function(response) {
+    return response.json()
+})
+.then (function(characters) {
+    const characterBar = document.getElementById('character-bar');
+
+    characters.forEach(function(character) {
+        const span = document.createElement('span')
+        span.textContent = character.name;
+
+
+        span.addEventListener("click", function() {
+            showCharacterDetails(character);
+    });
+
+    characterBar.append(span)
+        
+    });
+
+})
+.catch(function(error) {
+    console.log(error);
+})
+
+characterBar.addEventListener('click', function(e) {
+
+})

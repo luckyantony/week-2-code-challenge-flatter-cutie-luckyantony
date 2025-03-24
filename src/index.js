3540eda928bc8809a3f1a958db7d95cd3b1f1dda
luckyantony <luckyantonyofficial@gmail.com>
2025-03-24 23:40:30 +0300

    Adding comments on my code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", main);
 
+// Here we fetch the characters data
 function main() {
     fetch("https://fluttercuties-db.vercel.app/characters")
         .then(function (response) {
@@ -8,10 +9,14 @@ function main() {
         .then(function (characters) {
             const characterBar = document.getElementById('character-bar');
 
+
+            // Here we loop through the characters and we create spans
             characters.forEach(function (character) {
                 const span = document.createElement('span');
                 span.textContent = character.name;
 
+
+                // Here we add a click event listener to show character details
                 span.addEventListener("click", function () {
                     showCharacterDetails(character);
                 });
@@ -24,22 +29,27 @@ function main() {
         });
 
     
+    // This handles the vote form submission
     const votesForm = document.getElementById("votes-form");
     votesForm.addEventListener("submit", function (e) {
-        e.preventDefault();
+        e.preventDefault(); //This prevents the form from reloading
+
+
+        //This gets the current vote count and the new votes
         const voteCount = document.getElementById("vote-count");
         const votesInput = document.getElementById("votes");
 
         const currentVotes = voteCount.textContent;
         const newVotes = votesInput.value;
 
+        // This updates the vote count
         voteCount.textContent = +currentVotes + +newVotes;
 
         // Clear input
         votesInput.value = "";
     });
 
-    // Handle reset button click
+    // This is for the reset button click event
     const resetBtn = document.getElementById("reset-btn");
     resetBtn.addEventListener("click", function () {
         const voteCount = document.getElementById("vote-count");
@@ -47,6 +57,8 @@ function main() {
     });
 }
 
+
+// This displays the character details
 function showCharacterDetails(character) {
     const name = document.getElementById("name");
     const image = document.getElementById("image");

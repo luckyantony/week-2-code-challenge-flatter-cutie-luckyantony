1f13e50711f2c9a2993ebb23a1f2f2afe67a178a
luckyantony <luckyantonyofficial@gmail.com>
2025-03-24 09:04:17 +0300

    Showing characters

--- src/index.js
+++ src/index.js
@@ -1,31 +1,62 @@
 // Your code here
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener("DOMContentLoaded", main)
+function main() {
 
-fetch ("http://localhost:3000/characters")
-.then(function(response) {
-    return response.json()
-})
-.then (function(characters) {
-    const characterBar = document.getElementById('character-bar');
+    fetch ("http://localhost:3000/characters")
+    .then(function(response) {
+        return response.json()
+    })
+    .then (function(characters) {
+        const characterBar = document.getElementById('character-bar');
 
-    characters.forEach(function(character) {
-        const span = document.createElement('span')
-        span.textContent = character.name;
+        characters.forEach(function(character) {
+            const span = document.createElement('span')
+            span.textContent = character.name;
 
 
-        span.addEventListener("click", function() {
-            showCharacterDetails(character);
-    });
+            span.addEventListener("click", function() {
+                showCharacterDetails(character);
+        });
 
-    characterBar.append(span)
+        characterBar.append(span)
         
-    });
+        });
 
-})
-.catch(function(error) {
-    console.log(error);
-})
+    })
+    .catch(function(error) {
+        console.log(error);
 
-characterBar.addEventListener('click', function(e) {
+    const votesForm = document.getElementById("votes-form")
+    votesForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const voteCount = document.getElementById("vote-count");
+        const votes = document.getElementById("votes");
 
-})
+
+        const currentVotes = voteCount.textContent;
+
+        const newVotes = votesInput.value
+
+        voteCount.textContent = +currentVotes + +newVotes;
+
+        votesInput.value = "";
+
+
+    })
+
+
+    })
+
+    function showCharacterDetails(character) {
+        const name = document.getElementById("name")
+        const Image = document.getElementById("image")
+        const voteCount = document.getElementById("vote-count")
+
+        name.textContent = character.name;
+        Image.src = character.Image;
+        voteCount.textContent = character.votes;
+    }
+
+};
+
+
